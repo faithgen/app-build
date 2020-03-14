368e487540f6e65201379dd3b47efeef9e5064c7
innoflash <innocentmazando@gmail.com>
2020-03-14 22:23:44 +0200

    added builds migration

---
+++ database/migrations/2020_03_14_222110_create_build_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('build_requests', function (Blueprint $table) {
+            $table->string('id')->index();
+            $table->string('ministry_id', 150)->index();
+            $table->boolean('release');
+            $table->timestamps();
+
+            $table->foreign('ministry_id')->references('id')->on('ministries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('build_requests');
+    }
+}
