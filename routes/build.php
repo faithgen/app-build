f7c824ed4d168880e8ea2d21d89edea434b7ed25
innoflash <innocentmazando@gmail.com>
2020-04-04 11:58:31 +0200

    moved modules to public build

--- routes/build.php
+++ routes/build.php
@@ -1,1 +1,19 @@
 <?php
+
+use Faithgen\AppBuild\Http\Controllers\ModuleController;
+use Faithgen\AppBuild\Http\Controllers\TemplateController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('modules')
+    ->group(function () {
+        Route::get('comments/{module}', [ModuleController::class, 'comments']);
+        Route::post('comment', [ModuleController::class, 'comment']);
+    });
+
+Route::prefix('templates')
+    ->group(function () {
+        Route::get('', [TemplateController::class, 'index']);
+        Route::get('{template}', [TemplateController::class, 'show']);
+        Route::get('comments/{template}', [TemplateController::class, 'comments']);
+        Route::post('comment', [TemplateController::class, 'comment']);
+    });
--- routes/source.php
+++ routes/source.php
@@ -20,16 +20,4 @@ Route::prefix('builds/')
         Route::get('{build}', [BuildController::class, 'buildLogs']);
     });
 
-Route::prefix('modules')
-    ->group(function () {
-        Route::get('comments/{module}', [ModuleController::class, 'comments']);
-        Route::post('comment', [ModuleController::class, 'comment']);
-    });
 
-Route::prefix('templates')
-    ->group(function () {
-        Route::get('', [TemplateController::class, 'index']);
-        Route::get('{template}', [TemplateController::class, 'show']);
-        Route::get('comments/{template}', [TemplateController::class, 'comments']);
-        Route::post('comment', [TemplateController::class, 'comment']);
-    });
